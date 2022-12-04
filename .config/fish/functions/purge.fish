e45a8822705a45dfb94aae19fbc919eeb36456ae
Saúl Nogueras <susensio@nuc>
2022-12-04 17:40:38 +0100

    Add pip fisher and nvim to upgrade function

--- .config/fish/functions/purge.fish
+++
@@ -1,3 +0,0 @@
-function purge --description 'Purge previously only removed packages'
-  dpkg -l | grep '^rc' | awk '{print $2}' | sudo xargs --no-run-if-empty dpkg --purge
-end
--- .config/fish/functions/upgrade.fish
+++ .config/fish/functions/upgrade.fish
@@ -1,7 +1,22 @@
 function upgrade --description 'Upgrade system'
+  __upgrade_apt
+  __purge_apt
+  fisher update
+  __packersync_nvim
+  pip cache purge
+end
+
+function __upgrade_apt
   sudo apt update &&
   sudo apt full-upgrade -y &&
   sudo apt autoremove -y &&
   sudo apt autoclean -y
-  purge
+end
+
+function __purge_apt --description 'Purge previously only removed packages'
+  dpkg -l | grep '^rc' | awk '{print $2}' | sudo xargs --no-run-if-empty dpkg --purge
+end
+
+function __packersync_nvim
+  command nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
 end
