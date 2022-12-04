454f5ccb299ede21948d7977ef4798bdd57d509a
Saúl Nogueras <susensio@nuc>
2022-12-04 17:41:06 +0100

    Always color tree

--- .config/fish/functions/tree.fish
+++ .config/fish/functions/tree.fish
@@ -1,6 +1,6 @@
 function tree --wraps=tree --description 'Tree contents in directory'
   if command -qs exa
-    exa --tree --icons $argv
+    exa --tree --icons --color always $argv
   else
     command tree $argv
   end
