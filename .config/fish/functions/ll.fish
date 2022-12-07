b713ccec4d7b99bf3117557cc0d19642e76ed3a6
Saúl Nogueras <susensio@nuc>
2022-12-07 13:07:48 +0100

    Tabs to spaces

--- .config/fish/functions/ll.fish
+++ .config/fish/functions/ll.fish
@@ -1,6 +1,3 @@
-#
-# These are very common and useful
-#
 function ll --wraps ls --description "List contents of directory using long format"
     ls -lha $argv
 end
--- .config/fish/functions/ls.fish
+++ .config/fish/functions/ls.fish
@@ -1,8 +1,8 @@
 function ls --wraps=exa --description 'List contents in directory'
-	if command -qs exa
-		exa --group-directories-first --icons $argv
-	else
-		source /usr/share/fish/functions/ls.fish
-		command ls $argv
-	end
+  if command -qs exa
+    exa --group-directories-first --icons $argv
+  else
+    source /usr/share/fish/functions/ls.fish
+    command ls $argv
+  end
 end
