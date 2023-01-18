e6cae8bb86e5ba3007642b8e6b52b5d5d589635e
Saúl Nogueras <susensio@nuc>
2023-01-18 15:04:25 +0100

    Change fish venv function to search local venvs

--- .config/fish/functions/venv.fish
+++ .config/fish/functions/venv.fish
@@ -1,4 +1,10 @@
 function venv --description 'Activates local python venv'
-  source $XDG_DATA_HOME/venv/bin/activate.fish
+  if test -d ".venv"
+    echo "Activating local venv..."
+    source .venv/bin/activate.fish
+  else
+    echo "Activating global venv..."
+    source $XDG_DATA_HOME/venv/bin/activate.fish
+  end
 end
 
