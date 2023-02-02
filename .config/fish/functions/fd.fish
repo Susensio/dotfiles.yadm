236eb98c23341c33b35dfc25153afe2fa2a1043c
Saúl Nogueras <susensio@nuc>
2023-02-02 18:53:08 +0100

    Maybe bugfix

--- .config/fish/functions/fd.fish
+++ .config/fish/functions/fd.fish
@@ -1,6 +1,6 @@
 function fd --wraps=fdfind --description 'alias fd=fdfind'
   if isatty stdout
-    command fdfind --color always $argv | less
+    command fdfind --color always $argv | less -XR
   else
     command fdfind $argv
   end
