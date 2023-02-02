36deb6ece7e83ed6137430285e255fb1a9dfac45
Saúl Nogueras <susensio@nuc>
2023-02-02 18:52:02 +0100

    Fish fzf use environment variable

--- .config/fish/conf.d/fzf.fish
+++ .config/fish/conf.d/fzf.fish
@@ -1,5 +1,5 @@
 # Same opts as system fzf
-set fzf_fd_opts --hidden --exclude=.git
+set fzf_fd_opts (string split -- ' ' $FZF_FD_OPTS)
 
 # Default bindings but bind Search Directory to Ctrl+F and Search Variables to Ctrl+Alt+V
 fzf_configure_bindings --directory=\cf --variables=\e\cv
