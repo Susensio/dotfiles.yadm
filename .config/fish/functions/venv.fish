3b41906ab7b1f55657a7ce6794aff46b4533e68c
Saúl Nogueras <susensio@nuc>
2023-02-02 18:55:29 +0100

    Use proper venv in xdg dirs

--- .config/fish/functions/venv.fish
+++ .config/fish/functions/venv.fish
@@ -1,7 +1,7 @@
 function venv --description 'Activates local python venv'
   if test -d ".venv"
     echo "Activating local venv..."
-    source .venv/bin/activate.fish
+    source .local/share/venv/bin/activate.fish
   else
     echo "Activating global venv..."
     source $XDG_DATA_HOME/venv/bin/activate.fish
