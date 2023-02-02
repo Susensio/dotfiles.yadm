cea7d4339939c8d8c9b00c0b5dca9935e4e105dd
Saúl Nogueras <susensio@nuc>
2023-02-02 18:52:38 +0100

    Better fish fzf

--- .config/fish/conf.d/fzf.fish
+++ .config/fish/conf.d/fzf.fish
@@ -1,9 +1,30 @@
 # Same opts as system fzf
 set fzf_fd_opts (string split -- ' ' $FZF_FD_OPTS)
 
-# Default bindings but bind Search Directory to Ctrl+F and Search Variables to Ctrl+Alt+V
-fzf_configure_bindings --directory=\cf --variables=\e\cv
+# Default bindings but bind Search Directory to Ctrl+F and Search Variables to Ctrl+Alt+V prepending $
+fzf_configure_bindings --directory=\cf --variables=
+function _fzf_search_variables_prepend
+  if not string match -q 'set*' (commandline) && test (commandline -t) != '$'
+    commandline --append '$'
+    commandline --cursor (math (commandline --cursor) + 1)
+  end
+  eval $_fzf_search_vars_command
+  if test (commandline) = '$'
+    commandline --replace ''
+  end
+end
+bind \e\cv _fzf_search_variables_prepend
+# bind \e\cv "commandline --append '\$'; $_fzf_search_vars_command" 
 
 # Better dir and file previews
 set fzf_preview_dir_cmd preview
 set fzf_preview_file_cmd preview
+
+# Edit with nvim on search-directory
+set fzf_directory_opts --bind "ctrl-e:execute($EDITOR {} &> /dev/tty)"
+
+# Exclude the command timestamp from the search scope when in Search History
+set fzf_history_opts "--nth=4.."
+
+# Make history respect chronological order
+set fzf_history_opts --no-sort
