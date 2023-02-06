25605fe307014c7190111cbde527cffe144f96b1
Saúl Nogueras <susensio@nuc>
2023-02-06 15:52:51 +0100

    Fix lazy yadm hook

--- .config/fish/functions/ly.fish
+++ .config/fish/functions/ly.fish
@@ -1,3 +1,5 @@
 function ly --wraps=lazygit --description 'Lazygit for yadm repo'
+  # Call yadm status to ensure hook execution
+  yadm status > /dev/null
   command lazygit --use-config-file "$XDG_CONFIG_HOME/yadm/lazygit.yml,$XDG_CONFIG_HOME/lazygit/config.yml" --work-tree ~ --git-dir ~/.local/share/yadm/repo.git
 end
