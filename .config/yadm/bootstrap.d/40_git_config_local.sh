33c3224a2e26a8fe7e94ff196a52e1ca31c9c542
Saúl Nogueras <susensio@nuc>
2023-09-15 18:39:18 +0200

    Add git config

---
+++ .config/yadm/bootstrap.d/40_git_config_local.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+git_config_folder=${XDG_CONFIG_HOME:-$HOME/.config}/git
+git_config_local_file=${git_config_folder}/config.local
+
+if [ ! -f $git_config_local_file]; then
+    cat >> $git_config_local_file << EOF
+[user]
+    email = $USER@$(hostname)
+EOF
+fi
