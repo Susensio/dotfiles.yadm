4d1abb2db3cf92f7dd555488f59943891943e8c9
Saúl Nogueras <susensio@nuc>
2023-09-19 19:46:42 +0200

    Add anacrontab bootstrap

---
+++ .config/yadm/bootstrap.d/60_anacron.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+# Create folders if neccesary
+mkdir -p "${XDG_CONFIG_HOME:-~./config}/anacron"
+mkdir -p "${XDG_STATE_HOME:-~./local/state}/anacron"
+
+if ! crontab -l; then
+  cat << EOF | crontab -
+XDG_CONFIG_HOME="~/.config"
+XDG_STATE_HOME="~.local/state"
+
+# m h  dom mon dow  command
+EOF
+fi
+
+if ! crontab -l | grep "anacron" &> /dev/null; then
+  echo "Creating user crontab for anacron..."
+  job='@hourly /usr/sbin/anacron -t "${XDG_CONFIG_HOME}/anacron/anacrontab" -S "${XDG_STATE_HOME}/anacron'
+  (crontab -l 2> /dev/null; echo "${job}") | crontab -
+fi
