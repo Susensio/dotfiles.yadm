ee6edeaa3c4f175aaa51ba184574bc6849d62763
Saúl Nogueras <susensio@nuc>
2023-09-20 10:19:09 +0200

    Better gh repo

--- .config/yadm/bootstrap.d/20_gh_releases.sh
+++ .config/yadm/bootstrap.d/20_gh_releases.sh
@@ -3,4 +3,4 @@ set -eu -o pipefail
 
 while IFS=\; read cmd repo; do
   ~/bin/download-gh-release --repo "${repo}" --cmd "${cmd}"
-done < requirements.gh
+done < <(cat requirements*.gh)
--- .config/yadm/bootstrap.d/30_update_repos.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-mkdir -p ~/.local/bin
-mkdir -p "${XDG_DATA_HOME}/man/man1"
-
-PATH=~/bin:$PATH update-repos
