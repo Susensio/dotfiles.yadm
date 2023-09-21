cfea06217b813b3aed9a9c3f7698380872da68a2
Saúl Nogueras <susensio@nuc>
2023-09-21 11:59:44 +0200

    Better bootstrap messages

--- .config/yadm/bootstrap.d/20_gh_releases.sh
+++ .config/yadm/bootstrap.d/20_gh_releases.sh
@@ -2,5 +2,6 @@
 set -eu -o pipefail
 
 while IFS=\; read cmd repo; do
+  echo "Installing repo for ${cmd}..."
   ~/bin/download-gh-release --repo "${repo}" --cmd "${cmd}"
 done < <(cat $(compgen -G "$(dirname -- $0)/requirements*.gh"))
