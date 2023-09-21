2d74884ea3b36d4eac448c1d504ad1ce4643ac48
Saúl Nogueras <susensio@nuc>
2023-09-21 12:51:21 +0200

    Faster gh-releases

--- .config/yadm/bootstrap.d/20_gh_releases.sh
+++ .config/yadm/bootstrap.d/20_gh_releases.sh
@@ -2,6 +2,8 @@
 set -eu -o pipefail
 
 while IFS=\; read cmd repo; do
-  echo "Installing repo for ${cmd}..."
-  ~/bin/download-gh-release --repo "${repo}" --cmd "${cmd}"
+  if ! command -v "${cmd}" &> /dev/null; then
+    echo "Installing repo for ${cmd}..."
+    ~/bin/download-gh-release --repo "${repo}" --cmd "${cmd}"
+  fi
 done < <(cat $(compgen -G "$(dirname -- $0)/requirements*.gh"))
