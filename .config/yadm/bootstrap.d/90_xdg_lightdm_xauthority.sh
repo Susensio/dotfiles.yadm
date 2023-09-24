c770d65cf471735fd01c788c9fe523e6a1dca805
Saúl Nogueras <susensio@nuc>
2023-09-24 11:22:34 +0200

    XDG Xauthority lightdm raspberry FIX

--- .config/yadm/bootstrap.d/90_xdg_lightdm_xauthority.sh
+++ .config/yadm/bootstrap.d/90_xdg_lightdm_xauthority.sh
@@ -4,5 +4,5 @@ conf='user-authority-in-system-dir'
 
 # https://askubuntu.com/questions/960793/whats-the-right-place-to-set-the-xauthority-environment-variable/961459#961459
 if [[ -f "${file}" ]]; then
-  sudo sed -i -e "/${conf}/s/^#//g" -e "/${conf}/s/=false/true/g" "${file}"
+  sudo sed -i -e "/#${conf}/s/^#//g" -e "/${conf}/s/=false/true/g" "${file}"
 fi
