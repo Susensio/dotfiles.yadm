6a37bdfd4e7ea448f04f561b5ceaabc8dfd11914
Saúl Nogueras <susensio@nuc>
2024-03-25 14:19:59 +0100

    List tree gitignore

--- .config/fish/functions/aliases/lt.fish
+++ .config/fish/functions/aliases/lt.fish
@@ -1,3 +1,3 @@
 function lt --wraps ll --description "List contents of directory using tree format"
-    ll --tree --git --ignore-glob=".git" $argv
+    ll --tree --git --ignore-glob=".git" --git-ignore $argv
 end
