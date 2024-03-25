f912f76b034fb0f1369eb183e86518395dd7fac0
Saúl Nogueras <susensio@nuc>
2024-03-25 13:55:37 +0100

    Fzf theme

--- .config/profile.d/fzf.sh
+++ .config/profile.d/fzf.sh
@@ -9,7 +9,7 @@ export FZF_DEFAULT_COMMAND="$FZF_FD_COMMAND_FILES"
 
 export FZF_BIND_SWITCH_FILES_DIRS="--bind 'ctrl-d:reload($FZF_FD_COMMAND_DIRS)' --bind 'ctrl-f:reload($FZF_FD_COMMAND_FILES)'"
 
-export FZF_DEFAULT_OPTS='--cycle --layout=reverse --ansi --border=bold --height=50% --preview-window="right:50%:wrap,<60(down:50%:wrap)" --marker="*" --bind tab:toggle+down,btab:toggle+up --bind "ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down" --bind "ctrl-y:execute-silent(printf {} | clipboard copy)" --bind "ctrl-V:transform-query(echo -n {q}; clipboard paste)" --bind "ctrl-w:backward-kill-word"'
+export FZF_DEFAULT_OPTS='--cycle --layout=reverse --ansi --border=bold --height=50% --preview-window="right:50%:wrap,<60(down:50%:wrap)" --marker="*" --bind tab:toggle+down,btab:toggle+up --bind "ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down" --bind "ctrl-y:execute-silent(printf {} | clipboard copy)" --bind "ctrl-V:transform-query(echo -n {q}; clipboard paste)" --bind "ctrl-w:backward-kill-word" --color=bg+:#1e2326,bg:#1e2326,spinner:#e69875,hl:#a7c080,fg:#9da9a0,header:#a7c080,info:#dbbc7f,pointer:#e69e80,marker:#e69875,fg+:#e4e1cd,prompt:#dbbc7f,hl+:#a7c080'
 
 # used on fish function that launchs fzf-tmux
 export FZF_TMUX_OPTS="-p90%,60%"
