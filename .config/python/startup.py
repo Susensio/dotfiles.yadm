48557e554fcee9d8aaff8d42ee8f9481534e29df
Saúl Nogueras <susensio@nuc>
2024-04-04 14:12:44 +0200

    FIX: Add python startup

---
+++ .config/python/startup.py
@@ -0,0 +1,35 @@
+# Store interactive Python shell history in ~/.local/state/python_history
+# script from xdg-ninja
+
+import os
+import atexit
+import readline
+from pathlib import Path
+
+if readline.get_current_history_length() == 0:
+
+    state_home = os.environ.get("XDG_STATE_HOME")
+    if state_home is None:
+        state_home = Path.home() / ".local" / "state"
+    else:
+        state_home = Path(state_home)
+
+    history_path = state_home / "python_history"
+    if history_path.is_dir():
+        raise OSError(f"'{history_path}' cannot be a directory")
+
+    history = str(history_path)
+
+    try:
+        readline.read_history_file(history)
+    except OSError: # Non existent
+        pass
+
+    def write_history(history):
+        import readline
+        try:
+            readline.write_history_file(history)
+        except OSError:
+            pass
+
+    atexit.register(write_history, history)
