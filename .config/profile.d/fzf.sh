31c2f4f9cf179a27a8199a324affd8e4885bc888
Saúl Nogueras <susensio@nuc>
2024-04-11 10:25:37 +0200

    Prettier fzf config

--- .config/profile.d/fzf.sh
+++ .config/profile.d/fzf.sh
@@ -6,15 +6,28 @@ export FZF_FD_OPTS_DIRS="--type d $FZF_FD_OPTS"
 export FZF_FD_COMMAND_FILES="fdfind --strip-cwd-prefix $FZF_FD_OPTS_FILES"
 export FZF_FD_COMMAND_DIRS="fdfind --strip-cwd-prefix $FZF_FD_OPTS_DIRS"
 export FZF_DEFAULT_COMMAND="$FZF_FD_COMMAND_FILES"
+#| sed -r 's/\[1/\[0/g'"
 
 export FZF_BIND_SWITCH_FILES_DIRS="--bind 'ctrl-d:reload($FZF_FD_COMMAND_DIRS)' --bind 'ctrl-f:reload($FZF_FD_COMMAND_FILES)'"
 
-export FZF_DEFAULT_OPTS='--cycle --layout=reverse --ansi --border=bold --height=50% --preview-window="right:50%:wrap,<60(down:50%:wrap)" --marker="*" --bind tab:toggle+down,btab:toggle+up --bind "ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down" --bind "ctrl-y:execute-silent(printf {} | clipboard copy)" --bind "ctrl-V:transform-query(echo -n {q}; clipboard paste)" --bind "ctrl-w:backward-kill-word" --color=bg+:#1e2326,bg:#1e2326,spinner:#e69875,hl:#a7c080,fg:#9da9a0,header:#a7c080,info:#dbbc7f,pointer:#e69e80,marker:#e69875,fg+:#e4e1cd,prompt:#dbbc7f,hl+:#a7c080'
+export FZF_DEFAULT_OPTS='--cycle '\
+'--layout=reverse '\
+'--ansi '\
+'--border=bold '\
+'--height=50% '\
+'--preview-window="right:50%:wrap,<60(down:50%:wrap)" '\
+'--marker="*" '\
+'--bind tab:toggle+down,btab:toggle+up '\
+'--bind "ctrl-u:preview-half-page-up,ctrl-d:preview-half-page-down" '\
+'--bind "ctrl-y:execute-silent(printf {} | clipboard copy)" '\
+'--bind "ctrl-V:transform-query(echo -n {q}; clipboard paste)" '\
+'--bind "ctrl-w:backward-kill-word" '\
+'--color=fg:#9da9a0,fg+:#e4e1cd,bg:#1e2326,bg+:#1e2326,hl:underline:#a7c080,hl+:underline:#a7c080,spinner:#e69875,header:#a7c080,info:#dbbc7f,pointer:#e69e80,marker:#e69875,prompt:#dbbc7f'
 
-# used on fish function that launchs fzf-tmux
-export FZF_TMUX_OPTS="-p90%,60%"
-# non standard
-export FZF_OPTS_PREVIEW="$FZF_DEFAULT_OPTS --preview='preview {}'"
-
-# zoxide
-export _ZO_FZF_OPTS="$FZF_DEFAULT_OPTS --preview='preview {2..}'"
+# # used on fish function that launchs fzf-tmux
+# export FZF_TMUX_OPTS="-p90%,60%"
+# # non standard
+# export FZF_OPTS_PREVIEW="$FZF_DEFAULT_OPTS --preview='preview {}'"
+#
+# # zoxide
+# export _ZO_FZF_OPTS="$FZF_DEFAULT_OPTS --preview='preview {2..}'"
