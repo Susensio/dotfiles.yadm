71c72d058727e07dd194195128bda171bb419f87
Saúl Nogueras <susensio@nuc>
2024-04-11 10:27:16 +0200

    Fish source newlines

--- .config/fish/conf.d/00_profile.fish
+++ .config/fish/conf.d/00_profile.fish
@@ -4,9 +4,16 @@ function _source -d "Translate export, path additions and subsequent sources fro
   # add_path VALUE    -> fish_add_path -g VALUE   # already checks if directory exists
   # source FILE       -> _source FILE          # use recursion
   # also conditional and loops are converted
+  argparse 'd/dryrun' -- $argv
+  set -l cmd source
+  if set -q _flag_dryrun
+    set cmd cat
+  end
+
   set -l file "$argv[1]"
   if test -f $file
-    command grep -P '^export|^\s*add_path|^\s*source|^if|^fi$|^\s*for|^\s*done$|^\w+=' $file |
+    awk '{if (sub(/\\\$/,"")) printf "%s", $0; else print $0}' $file |
+      command grep -P '^export|^\s*add_path|^\s*source|^if|^fi$|^\s*for|^\s*done$|^\w+=' |
       sed -E 's/^export (\w+)=/set -gx \1 /;
         s/^(\w+)=/set -l \1 /;
         s/\badd_path\b/fish_add_path -g -m/;
@@ -16,7 +23,7 @@ function _source -d "Translate export, path additions and subsequent sources fro
         s/\bdone\b/end/;
         s/;\s*do$//;
         s/\$\{([^}]*)\}/$\1/' |
-      source
+      $cmd
   end
 end
 
