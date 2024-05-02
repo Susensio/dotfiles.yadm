0c22db68bf99f0a7b914b984a7436501f7ffa212
Saúl Nogueras <susensio@nuc>
2024-05-02 15:22:55 +0200

    Add nvim quickfix delete keymap

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -437,3 +437,15 @@ map_ft("qf", "n", "q",
   end,
   { nowait = true, desc = "Close quickfix or loclist with `q`" }
 )
+
+-- https://github.com/benlubas/.dotfiles/blob/main/nvim%2Fafter%2Fftplugin%2Fqf.lua
+local del_qf_item = function()
+  local items = vim.fn.getqflist()
+  local line = vim.fn.line('.')
+  table.remove(items, line)
+  vim.fn.setqflist(items, "r")
+  vim.api.nvim_win_set_cursor(0, { line, 0 })
+end
+
+vim.keymap.set("n", "dd", del_qf_item, { silent = true, buffer = true, desc = "Remove entry from QF" })
+vim.keymap.set("v", "D", del_qf_item, { silent = true, buffer = true, desc = "Remove entry from QF" })
