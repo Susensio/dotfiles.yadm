e5e02d70a906af8668b6a8aafebbd46440704d70
Saúl Nogueras <susensio@nuc>
2024-05-17 09:40:59 +0200

    Fish better copy
    Feat: extend fish <C-x> copy to system clipboard, to also copy the
    killring if commandline is empty

--- .config/fish/conf.d/keybinds.fish
+++ .config/fish/conf.d/keybinds.fish
@@ -1,3 +1,5 @@
 # CTRL-E open nautilus
 bind \ce 'nautilus . &'
 
+# If nothing in the commandline, copy the killring to system clipboard (sync)
+bind \cx 'test -z "$(commandline)" && echo -n $fish_killring[1] | fish_clipboard_copy || fish_clipboard_copy'
