37b68a82240149b4dc6cfa67f20a2dc3ef85d6f6
Saúl Nogueras <susensio@nuc>
2024-06-17 13:08:05 +0200

    Better git

--- .config/nvim/lua/plugins/git.lua
+++ .config/nvim/lua/plugins/git.lua
@@ -1,15 +1,3 @@
-local function format_dotfiles_paths()
-   local os = require("utils.os")
-   local include = vim.fn.expand("$XDG_CONFIG_HOME") .. "/yadm/include"
-   local lines = os.read_lines(include)
-   lines = vim.tbl_map(os.expand_vars, lines)
-   -- fix glob to vim pattern
-   lines = vim.tbl_map(function(line)
-      return line:gsub("%*%*", "*")
-   end, lines)
-   return table.concat(lines, ",")
-end
-
 return {
    { -- mini.diff
       "echasnovski/mini.diff",
@@ -36,10 +24,15 @@ return {
 
    { -- baredot
       "ejrichards/baredot.nvim",
-      event = "BufRead " .. format_dotfiles_paths(),
+      event = {
+         "BufRead " .. vim.fn.expand("$XDG_CONFIG_HOME") .. "*",
+         "BufRead " .. vim.fn.expand("$HOME") .. "bin/*",
+      },
+      ft = 'oil',
       opts = {
          git_dir = "$XDG_DATA_HOME/yadm/repo.git",
          git_work_tree = "$HOME",
       }
    },
+
 }
