8d874ba0a952d2b3e832efbf3e61fef879695f14
Saúl Nogueras <susensio@nuc>
2024-06-27 11:12:10 +0200

    Add nvim operators

--- .config/nvim/lua/plugins/operators.lua
+++ .config/nvim/lua/plugins/operators.lua
@@ -6,4 +6,45 @@ return {
     },
     config = true,
   },
+
+  { -- mini.operators
+    "echasnovski/mini.operators",
+    init = function(plugin)
+      vim.keymap.set("n", "S", "s$", { noremap = true })
+    end,
+    keys = {
+      { mode = { "n", "x" }, "g=", desc = "Evaluate" },
+      { mode = { "n", "x" }, "gx", desc = "Exchange" },
+      { mode = { "n", "x" }, "gm", desc = "Multiply" },
+      { mode = { "n", "x" }, "s", desc = "Replace" },
+
+      -- { mode = "n", "gs", desc = "Sort" },
+    },
+    opts = {
+      evaluate = {
+        prefix = "g=",
+      },
+
+      -- Exchange text regions
+      exchange = {
+        prefix = "gx",
+      },
+
+      -- Multiply (duplicate) text
+      multiply = {
+        prefix = "gm",
+      },
+
+      -- Replace text with register (Substitute)
+      replace = {
+        prefix = "s",
+      },
+
+      -- Sort text
+      sort = {
+        -- prefix = "gs",
+        prefix = "",
+      }
+    },
+  },
 }
