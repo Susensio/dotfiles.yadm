3c8956c3f452d959d635fd802a466d426b6e9e0d
Saúl Nogueras <susensio@nuc>
2024-07-09 09:29:39 +0200

    Disable until fish is rusty

--- .config/fish/conf.d/keybinds.fish
+++ .config/fish/conf.d/keybinds.fish
@@ -7,10 +7,10 @@ bind \cx 'test -z "$(commandline)" && echo -n $fish_killring[1] | fish_clipboard
 # Accept suggestions like nvim copilot
 bind \ce forward-word
 # NOT WORKING, waiting for issue https://github.com/fish-shell/fish-shell/issues/10580
-bind \cl 'if test "$(commandline --current-buffer)" = "$(commandline --cut-at-cursor)";
-    tput reset; clear; commandline -f repaint;
-else;
-    commandline -f forward-char;
-end'
-bind \cl 'commandline --current-buffer'
+# bind \cl 'if test "$(commandline --current-buffer)" = "$(commandline --cut-at-cursor)";
+#     tput reset; clear; commandline -f repaint;
+# else;
+#     commandline -f forward-char;
+# end'
+# bind \cl 'commandline --current-buffer'
 bind \cj forward-char
