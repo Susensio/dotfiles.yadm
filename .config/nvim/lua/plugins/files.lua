48c2c630399685caf3e1316266cec14dd70d263e
Saúl Nogueras <snogueras@EM-SNOGUERAS>
2024-07-24 11:07:21 +0200

    Oil better lazyload

--- .config/nvim/lua/plugins/files.lua
+++ .config/nvim/lua/plugins/files.lua
@@ -104,11 +104,38 @@ return {
     'stevearc/oil.nvim',
     enabled = true,
     dependencies = { "nvim-tree/nvim-web-devicons" },
-    event = { "VimEnter */*,.*", "BufNew */*,.*" },
     cmd = { "Oil" },
-    init = function()
+    init = function(plugin)
       vim.keymap.set("n", "-", "<cmd>Oil<CR>", { desc = "Open parent directory" })
+
+      -- Lazy load
+      -- https://github.com/kevinm6/nvim/blob/0c2d0fcb04be1f0837ae8918b46131f649cba775/lua/plugins/editor/oil.lua#L141
+      if vim.fn.argc() == 1 then
+         local argv = tostring(vim.fn.argv(0))
+         local stat = vim.loop.fs_stat(argv)
+
+         local remote_dir_args = vim.startswith(argv, "ssh") or
+         vim.startswith(argv, "sftp") or
+         vim.startswith(argv, "scp")
+
+         if stat and stat.type == "directory" or remote_dir_args then
+            require("lazy").load { plugins = { plugin.name } }
+         end
+      end
+      if not require("lazy.core.config").plugins[plugin.name]._.loaded then
+         vim.api.nvim_create_autocmd("BufNew", {
+            callback = function()
+               if vim.fn.isdirectory(vim.fn.expand("<afile>")) == 1 then
+                  require("lazy").load { plugins = { "oil.nvim" } }
+                  -- Once oil is loaded, we can delete this autocmd
+                  return true
+               end
+            end,
+            once = true,
+         })
+      end
     end,
+
     opts = {
       default_file_explorer = true,
       keymaps = {
@@ -116,6 +143,7 @@ return {
       },
       delete_to_trash = true,
       skip_confirm_for_simple_edits = true,
+      prompt_save_on_select_new_entry = false,
       view_options = {
         show_hidden = true,
         natural_order = true,
