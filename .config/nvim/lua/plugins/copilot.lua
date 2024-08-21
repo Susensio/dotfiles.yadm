2ca277a662f16ac9dc63ca4ea0fb606633f6b5f1
Saúl Nogueras <susensio@nuc>
2024-08-21 11:06:00 +0200

    No need to auth copilot automatically
    It collides with bootstraping

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -3,7 +3,6 @@ return {
     "zbirenbaum/copilot.lua",
     enabled = true,
     cmd = "Copilot",
-    build = ":Copilot auth",
     main = "copilot",
     event = "InsertEnter",
     init = function(_)
@@ -17,24 +16,6 @@ return {
         end,
         { desc = "Toggle Copilot" }
       )
-
-      -- vim.keymap.set("i", "<Tab>", function()
-      --   local suggestion = require("copilot.suggestion")
-      --   -- not starting with a space
-      --   if suggestion.is_visible() then
-      --     local ns_id = vim.api.nvim_get_namespaces()["copilot.suggestion"]
-      --     local suggested_text = vim.api.nvim_buf_get_extmark_by_id(0, ns_id, 1, { details = true })[3].virt_text[1][1]
-      --     local starts_with_spaces = string.match(suggested_text, "^  +")
-      --     if starts_with_spaces ~= nil then
-      --       -- insert those spaces
-      --       vim.api.nvim_feedkeys(starts_with_spaces, "n", true)
-      --     else
-      --       suggestion.accept()
-      --     end
-      --   else
-      --     vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Tab>", true, false, true), "n", false)
-      --   end
-      -- end, { desc = "Super Tab" })
       vim.keymap.set("i", "<C-l>", function()
         local suggestion = require("copilot.suggestion")
         if suggestion.is_visible() then
