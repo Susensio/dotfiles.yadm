6190be5c0e1c74dc67cede5de8fe5de83cb35231
Saúl Nogueras <susensio@nuc>
2024-08-28 11:29:06 +0200

    Fix tmux environment

---
+++ .config/fish/conf.d/tmux_update_environment.fish
@@ -0,0 +1,8 @@
+function _tmux_update_environment --on-event="fish_prompt"
+  if set -q TMUX
+    tmux showenv \
+    | string match -re '^.?(?:SSH|DISPLAY)' \
+    | string replace -r '^([^-].*?)=(.*)' 'set -gx $1 $2' \
+    | string replace -r '^(-)(.*)' 'set -e $2'
+  end
+end
