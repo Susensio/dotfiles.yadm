68cbe454702a1ad221d1ca940d7479b05ae8bfb4
Saúl Nogueras <susensio@nuc>
2024-08-28 11:35:28 +0200

    Fix bug not sourcing

--- .config/fish/conf.d/tmux_update_environment.fish
+++ .config/fish/conf.d/tmux_update_environment.fish
@@ -4,5 +4,6 @@ function _tmux_update_environment --on-event="fish_prompt"
     | string match -re '^.?(?:SSH|DISPLAY)' \
     | string replace -r '^([^-].*?)=(.*)' 'set -gx $1 $2' \
     | string replace -r '^(-)(.*)' 'set -e $2'
+    | source
   end
 end
