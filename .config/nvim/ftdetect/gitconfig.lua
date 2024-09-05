209f1363294b0beea46de2cea2f0a3be6df753bf
Saúl Nogueras <susensio@nuc>
2024-09-05 12:35:58 +0200

    Better yadm cidm gitconfig

--- .config/nvim/ftdetect/gitconfig.lua
+++ .config/nvim/ftdetect/gitconfig.lua
@@ -1,5 +1,7 @@
 vim.filetype.add({
    pattern = {
       ["${XDG_CONFIG_HOME}/git/.+"] = "gitconfig",
+      ["${XDG_CONFIG_HOME}/yadm/gitconfig"] = "gitconfig",
+      ["${XDG_CONFIG_HOME}/cidm/gitconfig"] = "gitconfig",
    }
 })
