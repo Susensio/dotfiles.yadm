220fc82bc5483055009440197bc4b89c8d12c5b2
Saúl Nogueras <susensio@nuc>
2024-09-23 15:26:57 +0200

    Add spider - better nvim movements

--- .config/nvim/lua/plugins/targets.lua
+++ .config/nvim/lua/plugins/targets.lua
@@ -39,4 +39,33 @@ return {
       end,
    },
 
+   { -- spider
+      "chrisgrieser/nvim-spider",
+      event = "VeryLazy",
+      init = function()
+         vim.keymap.set(
+            { "n", "o", "x" },
+            "w",
+            "<cmd>lua require('spider').motion('w')<CR>",
+            { desc = "Spider-w" }
+         )
+         vim.keymap.set(
+            { "n", "o", "x" },
+            "e",
+            "<cmd>lua require('spider').motion('e')<CR>",
+            { desc = "Spider-e" }
+         )
+         vim.keymap.set(
+            { "n", "o", "x" },
+            "b",
+            "<cmd>lua require('spider').motion('b')<CR>",
+            { desc = "Spider-b" }
+         )
+      end,
+      opts = {
+         skipInsignificantPunctuation = true,
+         consistentOperatorPending = false,
+         subwordMovement = false,
+      }
+   }
 }
