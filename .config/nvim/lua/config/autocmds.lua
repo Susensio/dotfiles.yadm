ce8e7329b48b087717acf0fcc2e6dc12348fa8af
Saúl Nogueras <snogueras@em-snogueras>
2025-02-20 10:11:46 +0100

    Auto mkspell

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -193,3 +193,26 @@ autocmd("QuitPre", {
 --    group = user_grp,
 --    }
 -- )
+local maybe_mkspell = function(language)
+   local config_path = vim.fn.stdpath("config") -- Get Neovim's config path
+   local add_file = config_path .. "/spell/" .. language .. ".utf-8.add"
+   local spl_file = config_path .. "/spell/" .. language .. ".utf-8.add.spl"
+
+   if vim.fn.filereadable(add_file) == 1 then
+      local add_mtime = vim.fn.getftime(add_file) -- Get modification time of .add file
+      local spl_mtime = vim.fn.getftime(spl_file) -- Get modification time of .add.spl file
+
+      -- Run mkspell! only if .add is newer than .add.spl or .add.spl doesn't exist
+      if add_mtime > spl_mtime or spl_mtime == -1 then
+         vim.cmd("silent! mkspell! " .. spl_file .. " " .. add_file)
+      end
+   end
+end
+-- Ensure that the binary spl file is up-to-date with the source add file
+autocmd("VimEnter", {
+  pattern = "*",
+  callback = function()
+      maybe_mkspell("en")
+      maybe_mkspell("es")
+  end,
+})
