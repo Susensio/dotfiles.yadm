230dde880a54fc0f7fb1af02b939615519fd80ea
Saúl Nogueras <susensio@nuc>
2025-02-20 10:31:45 +0100

    Reorder nvim init

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,11 +1,12 @@
+safe_require = require("utils.require").safe
+
 vim.g.mapleader = " "
 vim.g.localmapleader = " "
 
-require("config.lazy")
-
-safe_require = require("utils.require").safe
-
 safe_require("config.options")
 safe_require("config.keymaps")
+
+safe_require("config.lazy")
+
 safe_require("config.commands")
 safe_require("config.autocmds")
