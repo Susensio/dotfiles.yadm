34ba9620c01e825fa85025204af7cde1092d70c7
Saúl Nogueras <susensio@nuc>
2026-01-25 16:13:55 +0100

    Better fish git prompt auto fetch

--- .config/fish/conf.d/auto_git_fetch.fish
+++ .config/fish/conf.d/auto_git_fetch.fish
@@ -1,5 +1,11 @@
 function _auto_git_fetch --on-variable PWD --description "git fetch automatically wherever inside a git repository"
   if test -d ".git"
+    if test -n (find -.git/FETCH_HEAD -mmin -5 2>/dev/null)
+      return
+    end
+    if not ping -c 1 -W 1 1.1.1.1 &>/dev/null
+      return
+    end
     git fetch --quiet &
   end
 end
