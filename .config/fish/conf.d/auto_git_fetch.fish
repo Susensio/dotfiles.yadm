c759119a0b0c36063a6699cf9467479e49e4fd78
Saúl Nogueras <susensio@gmail.com>
2026-08-16 21:16:28 +0200

    Fix fish auto-fetch glob typo

--- .config/fish/conf.d/auto_git_fetch.fish
+++ .config/fish/conf.d/auto_git_fetch.fish
@@ -1,6 +1,6 @@
 function _auto_git_fetch --on-variable PWD --description "git fetch automatically wherever inside a git repository"
   if test -d ".git"
-    if test -n (find -.git/FETCH_HEAD -mmin -5 2>/dev/null)
+    if test -n (find .git/FETCH_HEAD -mmin -5 2>/dev/null)
       return
     end
     if not ping -c 1 -W 1 1.1.1.1 &>/dev/null
