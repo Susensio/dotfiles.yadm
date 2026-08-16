e8aaea1b27a91cb20e7f09d439173c3f4f0444d6
Saúl Nogueras <susensio@gmail.com>
2026-08-16 21:16:57 +0200

    Use XDG bin/lib vars and relative symlinks in yadm bootstrap

--- .config/yadm/bootstrap.d/yadm.sh
+++ .config/yadm/bootstrap.d/yadm.sh
@@ -5,10 +5,10 @@ source "${HOME}/bin/log"
 
 REPO=yadm-dev/yadm
 
-BIN_HOME="${HOME}/.local/bin"
-LIB_HOME="${HOME}/.local/lib"
+BIN_HOME=${XDG_BIN_HOME:-$HOME/.local/bin}
+LIB_HOME=${XDG_LIB_HOME:-$HOME/.local/lib}
 MAN_HOME="${XDG_DATA_HOME:-$HOME/.local/share}/man"
-FISH_COMPLETIONS_DIR="${XDG_DATA_HOME:-$HOME/.local/share}/fish/vendor_completions.d"
+FISH_COMPLETIONS_DIR=${XDG_DATA_HOME:-$HOME/.local/share}/fish/vendor_completions.d
 mkdir --parents --verbose "$BIN_HOME" "$LIB_HOME" "$MAN_HOME/man1" "$FISH_COMPLETIONS_DIR"
 
 
@@ -22,16 +22,17 @@ if ! type -P yadm &> /dev/null; then
   fi
 
   log_info "Creating yadm symlink..."
-  ln -sfv "$(realpath "${LIB_HOME}"/yadm/yadm)" "${BIN_HOME}/"
+  ln -srfv "$(realpath "${LIB_HOME}"/yadm/yadm)" "${BIN_HOME}/"
 
   log_info "Updating manpages..."
-  ln -sfv "$(realpath "${LIB_HOME}"/yadm/yadm.1)" "${MAN_HOME}/man1/"
+  ln -srfv "$(realpath "${LIB_HOME}"/yadm/yadm.1)" "${MAN_HOME}/man1/"
 
   log_info "Updating fish completions..."
-  ln -sfv "$(realpath "${LIB_HOME}"/yadm/completion/fish/yadm.fish)" "$FISH_COMPLETIONS_DIR/"
+  ln -srfv "$(realpath "${LIB_HOME}"/yadm/completion/fish/yadm.fish)" "$FISH_COMPLETIONS_DIR/"
 fi
 
 # Version controlled gitconfig
 ${BIN_HOME}/yadm gitconfig include.path "${XDG_CONFIG_HOME:-${HOME}/.config}"/yadm/gitconfig
 # Do not pollute $HOME with github stuff
 ${BIN_HOME}/yadm -C $HOME sparse-checkout set --no-cone "/*" "!/README.md" "!/.github"
+
