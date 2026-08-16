9cf07733772c8516b547968e8aaeed7addf0edb0
Saúl Nogueras <susensio@gmail.com>
2026-08-16 21:16:57 +0200

    Tighten sudoers file permissions in yadm bootstrap

--- .config/yadm/bootstrap.d/xdg_compliance/sudo.sh
+++ .config/yadm/bootstrap.d/xdg_compliance/sudo.sh
@@ -5,4 +5,4 @@ SCRIPT_DIR="$(dirname "$(realpath "${BASH_SOURCE[0]}")")"
 ASSETS_DIR="${SCRIPT_DIR}/assets"
 CONFIG_FILE="/etc/sudoers.d/disable_admin_file"
 
-sudo install --mode 644 --compare -D --verbose "${ASSETS_DIR}${CONFIG_FILE}" "${CONFIG_FILE}"
+sudo install --mode 440 --compare -D --verbose "${ASSETS_DIR}${CONFIG_FILE}" "${CONFIG_FILE}"
