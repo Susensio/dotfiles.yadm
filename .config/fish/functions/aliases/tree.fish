1fc372eb7e9991501aaf35e13996cf99b93bfed6
Saúl Nogueras <susensio@gmail.com>
2026-08-16 21:16:29 +0200

    Respect gitignore in fish tree alias

--- .config/fish/functions/aliases/tree.fish
+++ .config/fish/functions/aliases/tree.fish
@@ -1,6 +1,6 @@
 function tree --wraps=eza --description 'Tree contents in directory'
     if command -qs eza
-        eza --tree --icons $argv
+        eza --tree --icons --git-ignore $argv
     else
         command tree $argv
     end
