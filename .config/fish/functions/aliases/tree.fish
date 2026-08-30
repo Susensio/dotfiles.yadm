2088e6927ff392a66f6941f56774019eb0bf9e29
Saúl Nogueras <susensio@gmail.com>
2026-08-30 07:25:54 +0200

    Improve fish tree alias to hide empty git-ignored roots

--- .config/fish/functions/aliases/tree.fish
+++ .config/fish/functions/aliases/tree.fish
@@ -1,6 +1,12 @@
 function tree --wraps=eza --description 'Tree contents in directory'
     if command -qs eza
-        eza --tree --icons --git-ignore $argv
+        # Do not show and empty tree for git-ignored root dir, only hide subdirs
+        set -l git_ignore
+        if not git check-ignore -- . $argv >/dev/null 2>&1
+            set git_ignore --git-ignore
+        end
+
+        eza --tree --group-directories-first --icons $git_ignore $argv
     else
         command tree $argv
     end
